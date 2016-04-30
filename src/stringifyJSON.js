945e437a2d677a5f81afb290b6ff5ed691f63fe7
Jae Shin <jae5464@gmail.com>
2016-04-30 09:41:29 +0900

    Revise comments and termination case for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -38,6 +38,7 @@ var stringifyJSON = function(obj) {
 
   var typesToSkip = ['undefined', 'function', 'symbol'];
 
+  // Recursive case
   // obj is an array
   if (Array.isArray(obj)) {
     var iteratorArr = function(result, item) {
@@ -62,37 +63,11 @@ var stringifyJSON = function(obj) {
     var innerString = _.reduce(obj, iteratorObj, '').slice(1);
     return '{' + innerString + '}';
   } else {
-    console.log('you missed a case!');
+    // Termination case
+    alert('Error: missed a case!');
   }
 
 };
 
-/*
-var stringifiableObjects = [
-  9,
-  null,
-  true,
-  false,
-  'Hello world',
-  [],
-  [8],
-  ['hi'],
-  [8, 'hi'],
-  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
-  [8, [[], 3, 4]],
-  [[[['foo']]]],
-  {},
-  {'a': 'apple'},
-  {'foo': true, 'bar': false, 'baz': null},
-  {'boolean, true': true, 'boolean, false': false, 'null': null },
-  // basic nesting
-  {'a': {'b': 'c'}},
-  {'a': ['b', 'c']},
-  [{'a': 'b'}, {'c': 'd'}],
-  {'a': [], 'c': {}, 'b': true}
-];
-
-console.log(stringifyJSON(stringifiableObjects[5]));
-*/
 
 
