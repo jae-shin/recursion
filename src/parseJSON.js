34bab7bff46c96b5eee1b6642c81ff1f1fd65e14
Jae Shin <jae5464@gmail.com>
2016-05-07 22:28:58 +0900

    Start implementation of parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,44 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  // length of original json
+  var length = json.length;
+
+  // current charactor position of the json
+  var charPos = 0;
+
+  var nextChar = function(charPos) {
+    return json[charPos + 1];
+  };
+
+  var parseString = function(str) {
+    return String(str);
+  };
+
+  var parseNumber = function(str) {
+    return Number(str);
+  };
+
+  // Figure out the top level value
+  switch (json[0]) {
+  case '\"':
+    return parseString(json.slice(1, length - 2));
+    break;
+  case '{':
+
+  default:
+    break;
+  }
+  
+  '\"'
+  '9' // number -, 0~9
+  '{'
+  '['
+  't' // true
+  'f' // false
+  'n' // null
+
+
+
 };
