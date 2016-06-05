7b64985249e0edf5056b7470d7fad480cfd0db3e
Jae Shin <jae5464@gmail.com>
2016-06-04 17:18:16 -0700

    Remove inner helper function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,33 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function(className, element) {
   // your code here
-  // document.body, element.childNodes, element.classList
+  // use document.body, element.childNodes, element.classList
+  element = element || document.body;
+
+  var result = [];
+
+  // Base Case
+  var classList = element.classList;
+  if (classList && classList.contains(className)) {
+    result.push(element);
+  }
+
+  var childNodes = element.childNodes;
   
-  var recursiveFunc = function(element, className) {
-    var result = [];
-
-    // check element itself
-    var classList = element.classList;
-    if (classList && classList.contains(className)) {
-      result.push(element);
-    }
-
-    var childNodes = element.childNodes;
-    
-    // Base case
-    // check if childNodes is null
-    if (!childNodes) {
-      return result;
-    }
-
-    // Recursive case
-    _.each(childNodes, function(item) {
-      result = result.concat(recursiveFunc(item, className));
+  if (childNodes) {
+    _.each(childNodes, function(child) {
+      result = result.concat(getElementsByClassName(className, child));
     });
-    return result;
-  };
+  }
 
-  return recursiveFunc(document.body, className);
+  return result;
 };
